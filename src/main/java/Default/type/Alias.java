d4095e2291aef1411af24c7c1866dc37320dae1c
Davide Veneto <davide.veneto@hotmail.it>
2020-06-24 10:27:05 +0200

    Aggiunto il file Alias

---
+++ src/main/java/Default/type/Alias.java
@@ -0,0 +1,23 @@
+package Default.type;
+
+public class Alias {
+
+    private String name;
+    private int id_refer;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId_refer() {
+        return id_refer;
+    }
+
+    public void setId_refer(int id_refer) {
+        this.id_refer = id_refer;
+    }
+}
