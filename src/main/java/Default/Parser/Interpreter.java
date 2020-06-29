ffa5c87e87f3f0c64f0c151b2c6215965feab516
Davide Veneto <davide.veneto@hotmail.it>
2020-06-29 19:38:52 +0200

    Completati comandi guarda e apri

--- src/main/java/Default/Parser/Interpreter.java
+++ src/main/java/Default/Parser/Interpreter.java
@@ -2,23 +2,31 @@ package Default.Parser;
 
 import Default.GameDescription;
 import Default.type.Commands_logic;
+import Default.type.GameObject;
+import Default.type.gameObjectContainer;
 
 import java.io.PrintStream;
+import java.util.List;
+import java.util.Scanner;
 
 public class Interpreter {
 
-    private static final String NO_DOOR="Non hai ancora l'abilità di passare oltre i muri...";
-    private static final String NO_OBJ="In questa stanza non ci sono oggetti.";
+    private static final String NO_DOOR = "Non hai ancora l'abilità di passare oltre i muri...";
+    private static final String NO_OBJ = "In questa stanza non ci sono oggetti.";
     public static final String ARE_IN = "Sei nella ";
-    public static final String ABSENT_OBJ_A ="Questo oggetto: ";
+    public static final String ABSENT_OBJ_A = "Questo oggetto: ";
     public static final String ABSENT_OBJ_B = "\nnon è presente nella stanza.";
-    private static final String CLOSED_OBJ = "Non hai ancora la vista a raggi X.\neApri l'oggetto per vederne il contenuto.";
+    private static final String CLOSED_OBJ = "Non hai ancora la vista a raggi X.\nApri l'oggetto per vederne il contenuto.";
+    private static final String IN_CONTAINER = "Nel contenitore vedi: ";
+    private static final String NO_OPENABLE = "Questo oggetto non si può aprire.\n";
+    private static final String WHAT = "Cosa devo aprire?";
+    private static final String OPENED_CONT = "Il contenitore si è aperto!";
 
 
-    private static final short NORTH=0;
-    private static final short EAST=1;
-    private static final short SOUTH=2;
-    private static final short WEST=3;
+    private static final short NORTH = 0;
+    private static final short EAST = 1;
+    private static final short SOUTH = 2;
+    private static final short WEST = 3;
 
     //costanti per dare colore agli output
     public static final String ANSI_RESET = "\u001B[0m";
@@ -31,18 +39,20 @@ public class Interpreter {
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
-    public Interpreter(){
+
+    public Interpreter() {
     }
 
-    public void interpret(Commands_logic command_move, GameDescription g, PrintStream out){
+    public void interpret(Commands_logic command_move, GameDescription g, PrintStream out) {
 
-        int index=0;
+        int index = 0;
+        int index_obj;
 
         //if(g.getLogic().get(index).equals(command_move)){
-        if(command_move.equals(g.getLogic().get(index))){
+        if (command_move.equals(g.getLogic().get(index))) {
             OutDescription(g.getLogic().get(index).getDescription());
 
-            switch (index+1){
+            switch (index + 1) {
                 case 2:
                     g.getCurrentRoom().getObjects().get(command_move.getObject_1().getID()).setVisible(true);
                     //g.getCurrentRoom().getObjects().forEach(game_object->game_object.setVisible(true));
@@ -99,15 +109,15 @@ public class Interpreter {
             }
 
             //questo if serve a far terminare il gioco
-            if(index==30){
+            if (index == 30) {
                 System.exit(0);
             }
             index++;
         }
-        if(g.getActions().containsValue(command_move.getAction())) {
+        if (g.getActions().containsValue(command_move.getAction())) {
             switch (command_move.getAction()) {
                 case "INVENTORY":
-                    if(!g.getInventory().isEmpty()) {
+                    if (!g.getInventory().isEmpty()) {
                         g.getInventory().forEach(game_object -> printString(game_object.getObjName()));
                     }
                     break;
@@ -119,7 +129,7 @@ public class Interpreter {
                             OutDescription(g.getCurrentRoom().getDoors().get(NORTH).getLock_Description());
                         } else {
                             OutDescription(g.getCurrentRoom().getDoors().get(NORTH).getDescription());
-                            g.setCurrentRoom(g.getRooms().get(g.getCurrentRoom().getDoors().get(NORTH).getNext_Room()-1));
+                            g.setCurrentRoom(g.getRooms().get(g.getCurrentRoom().getDoors().get(NORTH).getNext_Room() - 1));
                             out.println(ANSI_RED + ARE_IN + g.getCurrentRoom().getName() + ANSI_RESET);
                         }
                     }
@@ -133,7 +143,7 @@ public class Interpreter {
                             OutDescription(g.getCurrentRoom().getDoors().get(SOUTH).getLock_Description());
                         } else {
                             OutDescription(g.getCurrentRoom().getDoors().get(SOUTH).getDescription());
-                            g.setCurrentRoom(g.getRooms().get(g.getCurrentRoom().getDoors().get(SOUTH).getNext_Room()-1));
+                            g.setCurrentRoom(g.getRooms().get(g.getCurrentRoom().getDoors().get(SOUTH).getNext_Room() - 1));
                             out.println(ANSI_RED + ARE_IN + g.getCurrentRoom().getName() + ANSI_RESET);
                         }
                     }
@@ -146,7 +156,7 @@ public class Interpreter {
                             OutDescription(g.getCurrentRoom().getDoors().get(EAST).getLock_Description());
                         } else {
                             OutDescription(g.getCurrentRoom().getDoors().get(EAST).getDescription());
-                            g.setCurrentRoom(g.getRooms().get(g.getCurrentRoom().getDoors().get(EAST).getNext_Room()-1));
+                            g.setCurrentRoom(g.getRooms().get(g.getCurrentRoom().getDoors().get(EAST).getNext_Room() - 1));
                             out.println(ANSI_RED + ARE_IN + g.getCurrentRoom().getName() + ANSI_RESET);
                         }
                     }
@@ -159,23 +169,32 @@ public class Interpreter {
                             OutDescription(g.getCurrentRoom().getDoors().get(WEST).getLock_Description());
                         } else {
                             OutDescription(g.getCurrentRoom().getDoors().get(WEST).getDescription());
-                            g.setCurrentRoom(g.getRooms().get(g.getCurrentRoom().getDoors().get(WEST).getNext_Room()-1));
+                            g.setCurrentRoom(g.getRooms().get(g.getCurrentRoom().getDoors().get(WEST).getNext_Room() - 1));
                             out.println(ANSI_RED + ARE_IN + g.getCurrentRoom().getName() + ANSI_RESET);
                         }
                     }
                     break;
                 case "OPEN":
-
-                    if(g.getCurrentRoom().getObjects().contains(command_move.getObject_1())){
-                        if(command_move.getObject_1().isIs_container() && !command_move.getObject_1().isOpen()
-                                && command_move.getObject_1().isOpenable()){
-                            g.getCurrentRoom().getObjects().get(command_move.getObject_1().getID()).setOpen(true);
-                            //TODO
-
+                    //index_obj=getItemID(g.getCurrentRoom().getObjects(),command_move.getObject_1().getObjName());
+                        if(command_move.getObject_1()==null){
+                            out.println(WHAT);
+                        }else{
+                            index_obj=getItemID(g.getCurrentRoom().getObjects(),command_move.getObject_1().getObjName());
+                            if(command_move.getObject_1() instanceof gameObjectContainer){
+                                if(g.getCurrentRoom().getObjects().get(index_obj).isOpenable()
+                                        && !g.getCurrentRoom().getObjects().get(index_obj).isOpen() ){
+                                    g.getCurrentRoom().getObjects().get(index_obj).setOpen(true);
+                                    out.println(OPENED_CONT);
+                                }
+                            }else{
+                                out.print(NO_OPENABLE);
+                            }
 
                         }
-                    }
-                    out.println(ABSENT_OBJ_A + g.getCurrentRoom().getObjects().get(command_move.getObject_1().getID()).getAlias().get(0) + ABSENT_OBJ_B);
+                        if (g.getCurrentRoom().getObjects().contains(command_move.getObject_1())) {
+                            //if(g.)
+                        }
+                    //out.println(ABSENT_OBJ_A + g.getCurrentRoom().getObjects().get(command_move.getObject_1().getID()).getAlias().get(0) + ABSENT_OBJ_B);
                     break;
                 case "CLOSE":
                     break;
@@ -194,40 +213,33 @@ public class Interpreter {
                 case "USE":
                     break;
                 case "LOOK":
-                    if(command_move.getObject_1()==null){
+                    //index_obj=getItemID(g.getCurrentRoom().getObjects(), command_move.getObject_1().getObjName());
+                    if (command_move.getObject_1() == null) {
                         out.println(g.getCurrentRoom().getDescription());
-                        if(!g.getCurrentRoom().getObjects().isEmpty()) {
-                            g.getCurrentRoom().getObjects().forEach(game_object -> printString(game_object.getAlias().get(0)));
-                        /*
-                        for (GameObject gameObject: g.getCurrentRoom().getObjects() ) {
-                            if(gameObject instanceof gameObjectContainer){
-                                out.println("Questo oggetto-->" + gameObject.getAlias().get(0)+ " contiene: ");
-                                ((gameObjectContainer) gameObject).getContainerList().forEach(gameObject1 -> printString(gameObject1.getAlias().get(0)));
-                            }
-
-
-
-                        }
-
-                         */
-                        }else{
+                        if (g.getCurrentRoom().getObjects().isEmpty()) {
+                            //g.getCurrentRoom().getObjects().forEach(game_object -> printString(game_object.getAlias().get(0)));
                             out.println(NO_OBJ);
                         }
-                    }else{
-                        if(g.getCurrentRoom().getObjects().contains(command_move.getObject_1())){
-                            //TODO cercare di prendere l'oggetto corrispondente nella lista di oggetti della stanza senza considerare l'id
-                            if(g.getCurrentRoom().getObjects().get(command_move.getObject_1().getID()).isOpen()){
+                    } else {
+                        index_obj=getItemID(g.getCurrentRoom().getObjects(),command_move.getObject_1().getObjName());
+                        if (g.getCurrentRoom().getObjects().get(index_obj).getObjName().equals(command_move.getObject_1().getObjName())) {
 
+                            if (g.getCurrentRoom().getObjects().get(index_obj).isOpenable()
+                                    && g.getCurrentRoom().getObjects().get(index_obj).isOpen()) {
+                                out.println(IN_CONTAINER);
+                                if(g.getCurrentRoom().getObjects().get(index_obj) instanceof gameObjectContainer){
+                                    ((gameObjectContainer) g.getCurrentRoom().getObjects().get(index_obj)).getContainerList().forEach
+                                            (gameObject -> printString(gameObject.getAlias().get(0)));
+                                }
+                            } else if(!g.getCurrentRoom().getObjects().get(index_obj).isOpenable()){
+                                out.println(NO_OPENABLE);
                             }else{
                                 out.println(CLOSED_OBJ);
                             }
-
-                        }else{
-                            out.println(ABSENT_OBJ_A + command_move.getObject_1().getAlias().get(0)+ ABSENT_OBJ_B);
+                        } else {
+                            out.println(ABSENT_OBJ_A + command_move.getObject_1().getAlias().get(0) + ABSENT_OBJ_B);
                         }
                     }
-
-
 
                     break;
                 case "TURN_ON":
@@ -249,21 +261,34 @@ public class Interpreter {
                 case "CLIMB":
                     break;
             }
-        }else{
+        } else {
             out.println("Non ho capito!");
         }
     }
 
     //metodi di servizio
-    private void OutDescription(String s){
-        if(s!=null){
+    private void OutDescription(String s) {
+        if (s != null) {
             System.out.println(s);
         }
     }
+
     private static void printString(String str) {
         System.out.println(str);
     }
 
-    
+    private static int getItemID(List<GameObject> g_obj, String name) {
+        for(GameObject obj: g_obj){
+            if(obj.getObjName().equals(name)){
+                return g_obj.indexOf(obj);
+            }
+        }
+        return -1;
+    }
 
 }
+
+
+    
+
+
